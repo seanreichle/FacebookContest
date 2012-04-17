479cc9ab7ad4e6f8d9e61e497fb59df611293c98
Sean Reichle <Sean@heather_b.destination.dest-mark.com>
2012-04-17 09:31:49 -0700

    Initial add

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FacebookContentest::Application.config.secret_token = 'f7427b0bdc0c58aa90ed04e4f23db52e3c975c7ff076ca080e605234dfe078393676c71258337b3266eb5fc1b0c5b0dbe40ae69767ad4f659251ac272f4bb38a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+FacebookContentest::Application.config.session_store :cookie_store, key: '_FacebookContentest_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# FacebookContentest::Application.config.session_store :active_record_store
