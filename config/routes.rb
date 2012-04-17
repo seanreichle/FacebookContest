aee4a37301b0a8c98038a6e10090cba1fcfcc5ca
Sean Reichle <Sean@heather_b.destination.dest-mark.com>
2012-04-17 10:23:50 -0700

    Created users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 FacebookContentest::Application.routes.draw do
+  get "users/index"
+
+  get "users/new"
+
+  get "users/create"
+
+  get "users/show"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
