490541d90da833d62e41e44e6f5289dd3a4f5e16
Dave Laird <david.laird@destmark.com>
2012-04-20 15:12:49 -0700

    added the Image Uploader

--- app/uploaders/users_image_uploader.rb
+++ app/uploaders/users_image_uploader.rb
@@ -3,7 +3,7 @@
 class UsersImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
+  include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Include the Sprockets helpers for Rails 3.1+ asset pipeline compatibility:
@@ -11,13 +11,13 @@ class UsersImageUploader < CarrierWave::Uploader::Base
   # include Sprockets::Helpers::IsolatedHelper
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  # storage :file
+  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.first}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
@@ -36,15 +36,15 @@ class UsersImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :scale => [50, 50]
-  # end
+  version :thumb do
+    process :scale => [250, 250]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
--- config/initializers/a_constans.rb
+++ config/initializers/a_constans.rb
@@ -1,1 +1,3 @@
 EMAIL = YAML.load_file("#{Rails.root}/config/email_config.yml") if File.exist?("#{Rails.root}/config/email_config.yml")
+
+AWS = YAML.load_file("#{Rails.root}/config/s3.yml") if File.exist?("#{Rails.root}/config/s3.yml")
