d1fbeada6cbbe911a9ce162c2c7c3f67d66e3493
Dave Laird <david.laird@destmark.com>
2012-04-20 15:48:37 -0700

    Added uploaded image to the show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ActiveRecord::Base
-  attr_accessible :comment, :email, :first, :last, :zip_code
+  attr_accessible :comment, :email, :first, :last, :zip_code, :image
 
   validates :first, :presence => true
   validates :last, :presence => true
   validates :email, :presence => true, :uniqueness => true, :email => true
   validates :zip_code, :presence => true
   validates :comment, :presence => true
+  
+  mount_uploader :image, UsersImageUploader
 end
