ac8237062ef20fdb61248dc5b9415ffe956cb2be
Sean Reichle <mail@seanreichle.com>
2012-05-30 16:21:57 -0700

    wow

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,9 @@
 class User < ActiveRecord::Base
-  attr_accessible :comment, :email, :first, :last, :zip_code, :image, :image_cache
+  attr_accessible :comment, :email, :first, :zip_code, :image, :image_cache
 
   validates :first, :presence => true
   validates :email, :presence => true, :uniqueness => true, :email => true
   validates :zip_code, :presence => true
   validates :comment, :presence => true
-  
   mount_uploader :image, UsersImageUploader
-  
 end
