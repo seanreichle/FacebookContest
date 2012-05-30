b719c4a2034ca64de766ee09d02ee1de563073b1
Sean Reichle <mail@seanreichle.com>
2012-05-30 15:32:08 -0700

    Restoring files lost in merge

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120420221644) do
+ActiveRecord::Schema.define(:version => 20120418175535) do
 
   create_table "users", :force => true do |t|
     t.string   "first"
@@ -22,7 +22,6 @@ ActiveRecord::Schema.define(:version => 20120420221644) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.string   "zip_code"
-    t.string   "image"
   end
 
 end
